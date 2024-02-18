fe7e3d0e144db3baab3aa335ced09eedaef5a4e1
Stephan Ude <ude@mediatis.de>
2024-02-18 23:08:35 +0100

    MFR-72 global configuration schema

--- src/DistributorCoreInitialization.php
+++ src/DistributorCoreInitialization.php
@@ -2,6 +2,12 @@
 
 namespace DigitalMarketingFramework\Distributor\Core;
 
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\RenderingDefinition\RenderingDefinitionInterface;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\BooleanSchema;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\ContainerSchema;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\IntegerSchema;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\SchemaInterface;
+use DigitalMarketingFramework\Core\ConfigurationDocument\SchemaDocument\Schema\StringSchema;
 use DigitalMarketingFramework\Core\DataProcessor\Evaluation\EvaluationInterface;
 use DigitalMarketingFramework\Core\DataProcessor\ValueSource\ValueSourceInterface;
 use DigitalMarketingFramework\Core\Initialization;
@@ -36,6 +42,48 @@ class DistributorCoreInitialization extends Initialization
     ];
 
     protected const SCHEMA_MIGRATIONS = [];
+
+    protected function getGlobalConfigurationSchema(): ?SchemaInterface
+    {
+        $schema = new ContainerSchema();
+        $schema->getRenderingDefinition()->setLabel('Distributor Core');
+
+        // queue
+        $queueSchema = new ContainerSchema();
+        $queueSchema->getRenderingDefinition()->setNavigationItem(false);
+
+        $queueSchema->addProperty('pid', new IntegerSchema(0));
+
+        $expirationTimeSchema = new IntegerSchema(30);
+        $expirationTimeSchema->getRenderingDefinition()->setLabel('Expiration Time (days)');
+        $queueSchema->addProperty('expirationTime', $expirationTimeSchema);
+
+        $schema->addProperty('queue', $queueSchema);
+
+        // field upload
+        $fileUploadSchema = new ContainerSchema();
+        $fileUploadSchema->getRenderingDefinition()->setNavigationItem(false);
+
+        $fileUploadSchema->addProperty('disableProcessing', new BooleanSchema(false));
+
+        $fileUploadSchema->addProperty('baseUploadPath', new StringSchema('uploads/digital_marketing_framework/form_uploads/'));
+
+        $fileUploadSchema->addProperty('prohibitedExtension', new StringSchema('php,exe'));
+
+        $schema->addProperty('fileUpload', $fileUploadSchema);
+
+        // debug
+        $debugSchema = new ContainerSchema();
+        $debugSchema->getRenderingDefinition()->setNavigationItem(false);
+
+        $debugSchema->addProperty('enabled', new BooleanSchema(false));
+
+        $debugSchema->addProperty('file', new StringSchema('digital-marketing-framework-distributor-submission.log'));
+
+        $schema->addProperty('debug', $debugSchema);
+
+        return $schema;
+    }
 
     public function __construct(string $packageAlias = '')
     {
