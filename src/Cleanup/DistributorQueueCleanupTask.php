349256b81309f639dbe30c3ab6f523ac0968b012
Stephan Ude <ude@mediatis.de>
2025-06-26 09:14:16 +0200

    refactoring

--- src/Cleanup/DistributorQueueCleanupTask.php
+++ src/Cleanup/DistributorQueueCleanupTask.php
@@ -3,9 +3,7 @@
 namespace DigitalMarketingFramework\Distributor\Core\Cleanup;
 
 use DigitalMarketingFramework\Core\Cleanup\QueueCleanupTask;
-use DigitalMarketingFramework\Core\Queue\GlobalConfiguration\Settings\QueueSettings;
 use DigitalMarketingFramework\Core\Registry\RegistryInterface;
-use DigitalMarketingFramework\Distributor\Core\Queue\GlobalConfiguration\Settings\QueueSettings as DistributorQueueSettings;
 use DigitalMarketingFramework\Distributor\Core\Registry\RegistryInterface as DistributorRegistryInterface;
 
 class DistributorQueueCleanupTask extends QueueCleanupTask
@@ -13,11 +11,12 @@ class DistributorQueueCleanupTask extends QueueCleanupTask
     public function __construct(string $keyword, RegistryInterface $registry)
     {
         $distibutorRegistry = $registry->getRegistryCollection()->getRegistryByClass(DistributorRegistryInterface::class);
-        parent::__construct($keyword, $distibutorRegistry->getPersistentQueue());
-    }
 
-    protected function getQueueSettings(): QueueSettings
-    {
-        return $this->globalConfiguration->getGlobalSettings(DistributorQueueSettings::class);
+        $queueProcessor = $distibutorRegistry->getQueueProcessor(
+            $distibutorRegistry->getPersistentQueue(),
+            $distibutorRegistry->getDistributor()
+        );
+
+        parent::__construct($keyword, $queueProcessor);
     }
 }
